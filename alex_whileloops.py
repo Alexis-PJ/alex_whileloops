2dc6f16573dc5f0b8f3bcb084a5bd706242a8adc
Alexis-PJ <44810566+Alexis-PJ@users.noreply.github.com>
2019-01-10 08:39:44 -0700

    Add files via upload

---
+++ alex_whileloops.py
@@ -0,0 +1,45 @@
+'''
+1) swap two variables
+'''
+#a = 5
+#b = 10
+#print('a =', a,'b =', b)
+
+# create a temporary variable temp
+#temp = a # save the value of a in the temp variable
+#a = b # a is getting the value in b
+#b = temp
+
+#print('the new variables a =',a,'b =',b)
+
+# write all integers between 1 and 9 using while loop
+
+#x = 1
+#while x < 10:
+    #print(x,' ',end='')
+    #x =x +1
+#row = 1
+#col = 1
+
+#while row < 10:
+    #while col < 5:
+        #print(row,col,' ',end='')
+        #col = col + 1
+    #print()
+    #row = row + 1
+    #co1 = 1
+
+# fibonacci sequence
+# 1,,2,3,5,8,13,21,34,...
+
+x = 1
+y = 1
+cont = 1
+n = int(input('Enter how many fibonacci numbers you want'))
+while cont <= n:
+    print('a fibon number is:', x+y)
+
+    temp = x
+    x = y
+    y = y + temp
+    cont = cont + 1
