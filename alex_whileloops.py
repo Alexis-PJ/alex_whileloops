f3f30a7c03d0046d22dfc2aa07a418f6bcc31175
Alexis-PJ <44810566+Alexis-PJ@users.noreply.github.com>
2019-01-10 08:40:54 -0700

    Add files via upload

--- alex_whileloops.py
+++ alex_whileloops.py
@@ -18,28 +18,28 @@
 #while x < 10:
     #print(x,' ',end='')
     #x =x +1
-#row = 1
-#col = 1
+row = 1
+col = 1
 
-#while row < 10:
-    #while col < 5:
-        #print(row,col,' ',end='')
-        #col = col + 1
-    #print()
-    #row = row + 1
-    #co1 = 1
+while row < 10:
+    while col < 5:
+        print(row,col,' ',end='')
+        col = col + 1
+    print()
+    row = row + 1
+    co1 = 1
 
 # fibonacci sequence
 # 1,,2,3,5,8,13,21,34,...
 
 x = 1
 y = 1
-cont = 1
-n = int(input('Enter how many fibonacci numbers you want'))
-while cont <= n:
+cont = 'c'
+
+while cont == 'c':
     print('a fibon number is:', x+y)
 
     temp = x
     x = y
     y = y + temp
-    cont = cont + 1
+    cont = input('if want more fibon numbs press c, else any key')
